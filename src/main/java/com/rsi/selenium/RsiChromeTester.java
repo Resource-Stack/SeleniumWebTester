f9431c99b4dae242f690ccd77e02c20ba24dfeb9
= <mail.sparsha@gmail.com>
2020-11-04 14:04:00 -0500

    Fix input field populate issue

--- src/main/java/com/rsi/selenium/RsiChromeTester.java
+++ src/main/java/com/rsi/selenium/RsiChromeTester.java
@@ -372,17 +372,23 @@ public class RsiChromeTester {
 				driver.get(base_url);
 
 			element = fetchWebElement("INPUT", fieldName, xpath);
+			String isReadOnly = element.getAttribute("readonly");
+			if (isReadOnly != null && isReadOnly.equalsIgnoreCase("true")) {
+				setAttributeValue(element, "readonly", "0");
+				setAttributeValue(element, "value", inputValue);
+			} else {
+				element.sendKeys(inputValue);
+			}
 
-			setAttributeValue(element, "readonly", "0");
-			setAttributeValue(element, "value", inputValue);
 			// TODO this is failing for select.
 			// element.clear();
 
 			// (SAMEER 05302020) this can be an additional switch on an input field, if we
 			// should select the inputted value esp. in cases where inputted value has to be
 			// qualified from a dynamic list.
-			TimeUnit.SECONDS.sleep(2);
+
 			if (enterAction.equalsIgnoreCase("1")) {
+				TimeUnit.SECONDS.sleep(2);
 				element.sendKeys(Keys.ENTER);
 			}
 			status = checkStatus("");
